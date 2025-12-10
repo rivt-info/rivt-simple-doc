99fc6e64beb949e84cc630c54a82fccf2ace989d
rholland <rholland@structurelabs.com>
2025-12-09 19:27:05 -0800

    single doc rst2pdf update

--- rv000-single-doc.py
+++ rv000-single-doc.py
@@ -56,7 +56,7 @@ rv.V("""Beam Section Properties
 
     """)
 
-rv.V("""Forces and Stress
+rv.V("""Force and Stress
         
         m_1 <= omega_1 * S_1**2 / 8 | ftkips, mkN, 2 | mid-span UDL moment _[E]
 
@@ -81,17 +81,14 @@ rv.S("""Metadata
 
     """)
 
-rv.S("""Publish Doc 
+rv.D("""Publish Doc 
 
     _[[LAYOUT]]
     rv_docnameS = "Beam Moment"
     rv_headerL = ["date", "time", "file", "version"]
     _[[END]]
     
-    | ATTACH |
 
-    | PUBLISH | simpledoc.txt | text
-
-    | ATTACH |
+    | PUBLISH | rivt | html
 
     """)
